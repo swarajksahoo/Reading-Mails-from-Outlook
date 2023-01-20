77759ac7a2778c638669136c8a3ffe88980d92c6
swarajksahoo <swarajksahoo@gmail.com>
2023-01-20 10:12:59 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 outlook = win32com.client.Dispatch('outlook.application')
 mapi = outlook.GetNamespace("MAPI")
 
+#Get Account Details
 for account in mapi.Accounts:
     print(account.DeliveryStore.DisplayName)
 
